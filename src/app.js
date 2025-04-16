a5fcbee433f3ce1ec4d07b2d806898ea064ed39e
pradipgadhe333 <pradipgadhe333@gmail.com>
2025-04-16 16:39:12 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -16,5 +16,15 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 
+// routes import
+import userRouter from "./routes/user.route.js";
+
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
+
+
+
+// http://localhost:8000/api/v1/users/register
 
 export {app};
